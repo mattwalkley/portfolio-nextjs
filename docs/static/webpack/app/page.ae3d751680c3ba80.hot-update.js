a2dc06fac0198a02527db82a03f543da5bd0dfc1
Matthew Walkley <matthewwalkley@Matthews-MacBook-Air.local>
2025-09-22 11:07:42 -0500

    url encoded password

---
+++ docs/static/webpack/app/page.ae3d751680c3ba80.hot-update.js
@@ -0,0 +1,21 @@
+/*
+ * ATTENTION: An "eval-source-map" devtool has been used.
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file with attached SourceMaps in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+self["webpackHotUpdate_N_E"]("app/page",{
+
+/***/ "(app-pages-browser)/./node_modules/next/dist/build/webpack/loaders/next-flight-client-entry-loader.js?modules=%7B%22request%22%3A%22%2FUsers%2Fmatthewwalkley%2FProjects%2Fportfolio%2Fsrc%2Fapp%2Fcomponents%2FNavigation.tsx%22%2C%22ids%22%3A%5B%22default%22%5D%7D&server=false!":
+/*!************************************************************************************************************************************************************************************************************************************************************************!*\
+  !*** ./node_modules/next/dist/build/webpack/loaders/next-flight-client-entry-loader.js?modules=%7B%22request%22%3A%22%2FUsers%2Fmatthewwalkley%2FProjects%2Fportfolio%2Fsrc%2Fapp%2Fcomponents%2FNavigation.tsx%22%2C%22ids%22%3A%5B%22default%22%5D%7D&server=false! ***!
+  \************************************************************************************************************************************************************************************************************************************************************************/
+/***/ ((__unused_webpack_module, __unused_webpack_exports, __webpack_require__) => {
+
+eval(__webpack_require__.ts("Promise.resolve(/*! import() eager */).then(__webpack_require__.bind(__webpack_require__, /*! ./src/app/components/Navigation.tsx */ \"(app-pages-browser)/./src/app/components/Navigation.tsx\"));\n//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiKGFwcC1wYWdlcy1icm93c2VyKS8uL25vZGVfbW9kdWxlcy9uZXh0L2Rpc3QvYnVpbGQvd2VicGFjay9sb2FkZXJzL25leHQtZmxpZ2h0LWNsaWVudC1lbnRyeS1sb2FkZXIuanM/bW9kdWxlcz0lN0IlMjJyZXF1ZXN0JTIyJTNBJTIyJTJGVXNlcnMlMkZtYXR0aGV3d2Fsa2xleSUyRlByb2plY3RzJTJGcG9ydGZvbGlvJTJGc3JjJTJGYXBwJTJGY29tcG9uZW50cyUyRk5hdmlnYXRpb24udHN4JTIyJTJDJTIyaWRzJTIyJTNBJTVCJTIyZGVmYXVsdCUyMiU1RCU3RCZzZXJ2ZXI9ZmFsc2UhIiwibWFwcGluZ3MiOiJBQUFBLGdNQUE0SSIsInNvdXJjZXMiOlsiIl0sInNvdXJjZXNDb250ZW50IjpbImltcG9ydCgvKiB3ZWJwYWNrTW9kZTogXCJlYWdlclwiLCB3ZWJwYWNrRXhwb3J0czogW1wiZGVmYXVsdFwiXSAqLyBcIi9Vc2Vycy9tYXR0aGV3d2Fsa2xleS9Qcm9qZWN0cy9wb3J0Zm9saW8vc3JjL2FwcC9jb21wb25lbnRzL05hdmlnYXRpb24udHN4XCIpO1xuIl0sIm5hbWVzIjpbXSwiaWdub3JlTGlzdCI6W10sInNvdXJjZVJvb3QiOiIifQ==\n//# sourceURL=webpack-internal:///(app-pages-browser)/./node_modules/next/dist/build/webpack/loaders/next-flight-client-entry-loader.js?modules=%7B%22request%22%3A%22%2FUsers%2Fmatthewwalkley%2FProjects%2Fportfolio%2Fsrc%2Fapp%2Fcomponents%2FNavigation.tsx%22%2C%22ids%22%3A%5B%22default%22%5D%7D&server=false!\n"));
+
+/***/ })
+
+});
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,19 @@
+"use client";
 import Navigation from "./components/Navigation";
 import Footer from "./components/Footer";
 import Hero from "./components/Hero";
 import WhatIDo from "./components/WhatIDo";
 import CaseStudies from "./components/CaseStudies";
+import { useEffect } from "react";
 
 export default function Home() {
+  useEffect(() => {
+    const urlParams = new URLSearchParams(window.location.search);
+    const password = urlParams.get("pw");
+    if (password) {
+      localStorage.setItem("case-studies-password", password);
+    }
+  }, []);
   return (
     <div className="page">
       <Navigation />
