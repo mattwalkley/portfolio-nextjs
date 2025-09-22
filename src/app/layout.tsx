76b7e214c50244f6702f9d477356a607a267c311
Matthew Walkley <matthewwalkley@Matthews-MacBook-Air.local>
2025-09-22 12:32:35 -0500

    added ga

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,6 +27,23 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        {/* Google Analytics */}
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-BQGFEP3BM8"
+        />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-BQGFEP3BM8');
+            `,
+          }}
+        />
+      </head>
       <body className={inter.variable}>{children}</body>
     </html>
   );
