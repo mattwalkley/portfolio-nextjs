8d695c6ffb983181ec306ae8a0a9e3c8682c551d
Matthew Walkley <matthewwalkley@Matthews-MacBook-Air.local>
2025-10-02 09:21:29 -0500

    updated images

--- docs/types/app/case-studies/design-system/page.ts
+++ docs/types/app/case-studies/design-system/page.ts
@@ -1,8 +1,8 @@
-// File: /Users/matthewwalkley/Projects/portfolio/src/app/case-studies/draw-management/page.tsx
-import * as entry from '../../../../../src/app/case-studies/draw-management/page.js'
+// File: /Users/matthewwalkley/Projects/portfolio/src/app/case-studies/design-system/page.tsx
+import * as entry from '../../../../../src/app/case-studies/design-system/page.js'
 import type { ResolvingMetadata, ResolvingViewport } from 'next/dist/lib/metadata/types/metadata-interface.js'
 
-type TEntry = typeof import('../../../../../src/app/case-studies/draw-management/page.js')
+type TEntry = typeof import('../../../../../src/app/case-studies/design-system/page.js')
 
 type SegmentParams<T extends Object = any> = T extends Record<string, any>
   ? { [K in keyof T]: T[K] extends string ? string | string[] | undefined : never }
